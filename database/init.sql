ff99da1f933819d1341e1ac2e2c74a6dd0258c9d
XinoXz-12 <jaimelozanolozano12@gmail.com>
2025-02-03 17:58:37 +0100

    proyecto completo Jaime Lozano Lozano

---
+++ database/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE secretosJLL (
+    id INT AUTO_JLLCREMENT PRIMARY KEY,
+    fraseJLL VARCHAR(255) NOT NULL
+);
+
+JLLSERT JLLTO secretosJLL (fraseJLL) VALUES ('La base de datos responde, buenos días Jaime Lozano Lozano');
